a87f3d13e7c01826e409222d5f2dfb8dfc6e5d53
BostjanSkok <BostjanSkok2@gmail.com>
2013-10-30 09:23:35 +0100

    finished and graded

--- main.cpp
+++ main.cpp
@@ -12,8 +12,12 @@ struct fromKToN
 
 };
 
+int ThredsNum = 4;
+int fromInt = 0;
+int toInt = 100000;
+
 int totalPrimes=0;
-int totalSum=0;
+//int totalSum=0;
 
 int IsPrime(int num){
 
@@ -51,7 +55,7 @@ void *SumInterval(void *args)
 	int i = (params->k);
 	for(i; i <= (params->n); i++)
 	{
-		totalSum += i;
+		params->rez += i;
 	}
 	return NULL;
 }
@@ -93,55 +97,93 @@ void splitIntoThreads(int numOfThreads ,int from , int to )
 		totalPrimes += intervals[k].rez;
 
 	}
-	printf("%i", totalPrimes);
+	printf("With %i threads primes are %i \n",numOfThreads ,totalPrimes);
 }
 
 void NalogaA()
 {
 	pthread_t t1,t2;
 
-	struct fromKToN s1;
-	s1.k=1;
-	s1.n=100000;
+	struct fromKToN s1,s2;
+	s1.k=fromInt;
+	s1.n=toInt;
+	s1.rez = 0;
+
+	s2.k = fromInt;
+	s2.n = toInt;
+	s2.rez = 0;
 
 	if(pthread_create(&t1,NULL,HowManyPrimes,&s1)!=0)
 	perror("t1 create");
-	if(pthread_create(&t2,NULL,SumInterval,&s1)!=0)
+	if(pthread_create(&t2,NULL,SumInterval,&s2)!=0)
 	perror("t2 create");
 
 	if(pthread_join(t1,NULL)!=0)
 	perror("t1 join");
+	printf("NalogaA thread1 %i \n", s1.rez);
 	if(pthread_join(t2,NULL)!=0)
 	perror("t2 join");
+	printf("NalogaA thread2 %i \n", s2.rez);
+
 }
 
 void NoThreads()
 {
 	struct fromKToN s1;
-	s1.k=1;
-	s1.n=100000;
+	s1.k=fromInt;
+	s1.n=toInt;
+	s1.rez = 0;
 
 	HowManyPrimes((void*) &s1);
+	printf("NoThread primes  %i \n", s1.rez);
+	s1.rez = 0;
 	SumInterval((void*) &s1);
+	printf("NoThread sum of interval  %i \n", s1.rez);
+	
 }
 int main() {
-	DWORD dwStartTime = GetTickCount();
     DWORD dwElapsed;
 	clock_t start = clock();
-    splitIntoThreads(4,1,100);
+	DWORD dwStartTime = GetTickCount();
+    splitIntoThreads(ThredsNum,fromInt,toInt);
+	dwElapsed = GetTickCount() - dwStartTime;
+	printf("It took %d.%d seconds to complete SplitIntoThreads\n", dwElapsed / 1000, dwElapsed - dwElapsed / 1000);
+
+	clock_t end = clock();
+	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("%.10f \n",seconds);
+	 start = clock();
+
+    dwStartTime = GetTickCount();
     NalogaA();
+	dwElapsed = GetTickCount() - dwStartTime;
+	printf("It took %d.%d seconds to complete NalogaA()\n", dwElapsed / 1000, dwElapsed - dwElapsed / 1000);
+
+	 end = clock();
+	 seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("%.10f \n",seconds);
+
+	 start = clock();
+
+	dwStartTime = GetTickCount();
 	NoThreads();
+	dwElapsed = GetTickCount() - dwStartTime;
+	printf("It took %d.%d seconds to complete 	NoThreads \n", dwElapsed / 1000, dwElapsed - dwElapsed / 1000);
+
+	 end = clock();
+	 seconds = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("%.10f \n",seconds);
+
 
 
 	
-	clock_t end = clock();
-	 dwElapsed = GetTickCount() - dwStartTime;
+/*	clock_t end = clock();
 	float seconds = (float)(end - start) / CLOCKS_PER_SEC;
 	//printf("%.10f",seconds);
 
-	
+	*/
 
-    printf("It took %d.%3d seconds to complete\n", dwElapsed/1000, dwElapsed - dwElapsed/1000);
+  //  printf("It took %d.%3d seconds to complete\n", dwElapsed/1000, dwElapsed - dwElapsed/1000);
 	getchar();
 
 }
